5b331c4b5ff731629b7629c46f93c2ca1975f209
Geovane Silva <geovanesilva.hr@gmail.com>
2023-03-18 21:52:37 -0300

    DB and Form created!

--- hello.py
+++ hello.py
@@ -1,9 +1,39 @@
-from flask import Flask
+from flask import Flask, url_for
 from flask import render_template
+import sqlite3
 
+#Instanciando a aplicação
 app = Flask(__name__)
 
+#Criando o Bando de Dados
+connectionDB = sqlite3.connect("db/database.db")
+cursor = connectionDB.cursor()
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS  alunos(
+        matricula INT, 
+        p_nome VARCHAR(15),
+        s_nome VARCHAR(40),
+        email VARCHAR(80),
+        data_nasc INT,
+        cpf INT
+        )
+""")
+cursor.execute("""
+    CREATE TABLE IF NOT EXISTS end(
+        matricula INT,
+        logradouro VARCHAR(80),
+        numero INT,
+        cep INT
+    )
+""")    
 @app.route('/')
 @app.route('/<name>')
 def hello(name='Geovane'):
-    return render_template('hello.html', name=name)
+    return render_template('cadAlunos.html', name=name)
+
+def inserir(form):
+    cursor.executemany("""
+        INSERT INTO alunos VALUES(?, ?, ?, ?)
+    """, form)
+    return print("Inseriu!")
+
---
+++ readme.md
@@ -0,0 +1,14 @@
+# Projeto CRUD para Escolas  
+
+Olá!✌🏻 
+Este projeto consiste em um sistema de cadastro de Alunos, Cursos e Professores de uma Escola. Vou desenvolver do zero um CRUD utilizando:
+
+# Python 🐍
+    versão: ""
+    bibliotecas:
+
+# Flask 🌶
+    versão: ""
+
+# SQLite3 🟦
+    versão: ""
